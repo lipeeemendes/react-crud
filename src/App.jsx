ef21daa7d872494c980ac941d619600e8e33c55b
lipeeemendes <felipemendesofficail@gmail.com>
2023-06-13 14:10:17 -0300

    Criando funções

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
 import { useState } from "react";
 
 export default function App() {
+  const [names, setNames] = useState([]);
+  const [name, setName] = useState("");
 
-  const[names,setNames] = useState([])
-  const[name,setName]= useState("")
+  function handleClick() {
+    setNames([...names, name]);
+    setName("");
+  }
 
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleClick2() {
+    setName(name.filter((ele) => ele !== name));
+  }
 
   return (
     <>
       <h1>Todo</h1>
-      <input type="text" placeholder="write a task" />
-      <button>add</button>
-      <ul> 
-       {names.map((name) => (
-          <li key={name}>name</li>
+      <input
+        type="text"
+        placeholder="type a task..."
+        value={name}
+        onChange={handleChange}
+      />
+      <button onClick={handleClick}>add</button>
+      <ul>
+        {names.map((name) => {
+          <li key={name}>name</li>;
+        })}
       </ul>
     </>
   );
