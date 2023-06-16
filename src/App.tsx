26aae1bf115a4ade032af608e1a9b77fb3f97beb
lipeeemendes <felipemendesofficail@gmail.com>
2023-06-16 11:10:54 -0300

    Inclusão de tipagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
+
+type Task = {
+  id: string;
+  title: string;
+  status: boolean;
+};
 
 export default function App() {
-  const [tasks, setTasks] = useState(
-    () => JSON.parse(localStorage.getItem("tasks")) || []
+  const [tasks, setTasks] = useState<Task[] | []>(
+    () => JSON.parse(localStorage.getItem("tasks") as string) || []
   );
-  const [name, setName] = useState("");
+  const [name, setName] = useState<string>("");
 
   function handleClick() {
-    const newTodo = { id: crypto.randomUUID(), title: name, status: false };
+    const newTodo: Task = {
+      id: crypto.randomUUID(),
+      title: name,
+      status: false,
+    };
     setTasks([...tasks, newTodo]);
     setName("");
   }
 
-  function handleChange(e) {
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
     setName(e.target.value);
   }
 
-  function deleteTodo(id) {
-    const newList = tasks.filter((task) => task.id !== id);
+  function deleteTodo(id: string) {
+    const newList: Task[] = tasks.filter((task) => task.id !== id);
     setTasks([...newList]);
   }
 
-  function toggleTodo(id) {
-    const newList = tasks.map((task) => {
+  function toggleTodo(id: string) {
+    const newList: Task[] = tasks.map((task) => {
       if (task.id === id) {
         task.status = !task.status;
       }
